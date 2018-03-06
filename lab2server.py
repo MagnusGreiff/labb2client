c082f772f10ff3172003f43e98a48b56e512ef64
MagnusGreiff <mangegreiff@gmail.com>
2018-03-06 15:43:46 +0100

    meh

--- lab2server.py
+++ lab2server.py
@@ -32,6 +32,8 @@ while True:
             print("oh no, you lost the package: " + str(int(num[0])))
             lostPackages.append(int(num[0]))
             expectedPackage += 1
+            if count == int(amountSend[0]):
+                count = 0
         else:
             count += 1
             expectedPackage += 1
