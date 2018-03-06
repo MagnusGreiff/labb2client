4b0824e1884ec733c88998a692713c0dc30e1f38
MagnusGreiff <mangegreiff@gmail.com>
2018-03-06 16:14:24 +0100

    ylvagit add .

--- lab2server.py
+++ lab2server.py
@@ -7,39 +7,56 @@ serverSocket.bind(('', serverPort))
 
 print ("The UDP server is ready to recieve")
 
-prevNumber = 0
-count = 0
-start = 0
-expectedPackage = 0
+expectedPackage = []
+count = int (len(expectedPackage) -1)
 lostPackages = []
 
 while True:
-    message, clientAddress = serverSocket.recvfrom(1000)
-    # print(message, clientAddress)
-    print("Inside server")
-    if count < 1:
-        lostPackages = []
-        num = re.findall(':(\d+)', str(message))
-        start = int(num[0])
-        expectedPackage = int(num[0])
-    if message.decode() == 'closed':
-        count = 0
-
     try:
+        message, clientAddress = serverSocket.recvfrom(1000)
+        # print(message, clientAddress)
+        print("Inside server")
         num = re.findall(':(\d+)', str(message))
         amountSend = re.findall('(\d+):', str(message))
+        count += 1
+
+        package = num[0]
+
+        expectedPackage.append(package)
 
 
-        if expectedPackage == int(num[0]):
-            count += 1
-            expectedPackage += 1
-            if count == int(amountSend[0]):
-                count = 0
-        elif expectedPackage != int(num[0]):
-            print("oh no, you lost the package: " + str(int(num[0])))
-            lostPackages.append(int(num[0]))
-            expectedPackage += 1
-            count += 1
+        if expectedPackage[-1] == count:
+            print(package)
+        elif int(expectedPackage[int((len(expectedPackage)) - 2)]) == (int(expectedPackage[-1]) - 1):
+        # elif amountRecieved <= packageNum:
+            print(package)
+            # check number order
+        elif int(expectedPackage[int((len(expectedPackage)) - 2)]) != (int(expectedPackage[-1]) - 1):
+            print('skipped a package')
+
+    # if count < 1:
+    #     lostPackages = []
+    #     num = re.findall(':(\d+)', str(message))
+    #     start = int(num[0])
+    #     expectedPackage = int(num[0])
+    # if message.decode() == 'closed':
+    #     count = 0
+    #
+    # try:
+    #     num = re.findall(':(\d+)', str(message))
+    #     amountSend = re.findall('(\d+):', str(message))
+    #
+    #
+    #     if expectedPackage == int(num[0]):
+    #         count += 1
+    #         expectedPackage += 1
+    #         if count == int(amountSend[0]):
+    #             count = 0
+    #     elif expectedPackage != int(num[0]):
+    #         print("oh no, you lost the package: " + str(int(num[0])))
+    #         lostPackages.append(int(num[0]))
+    #         expectedPackage += 1
+    #         count += 1
 
     except IndexError:
         print("Restarting")
